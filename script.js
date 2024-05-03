b528b4167818a659e159772335e3d4906e7a10af
Essencia05 <re.letlape@gmail.com>
2024-05-03 16:13:23 +0200

    jscript

---
+++ script.js
@@ -0,0 +1,15 @@
+/*int sum=0;  problem 1*/         
+let sumP1=0;
+
+/* for(int i = 1; i<1000;I++) */
+for(let count= 1; count<1000; count++)
+{
+    if( count % 3 == 0||count % 5 == 0)
+    {
+        sumP1 = + 1;
+    }
+}
+console.log(sumP1);
+
+//problem 2 solution           
+//problem 3 solution
