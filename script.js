58e23e5047b8ccb0cf7d5c6a59bcd02b370328c4
Essencia05 <re.letlape@gmail.com>
2024-05-06 14:50:39 +0200

    footer

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-/*int sum=0;  problem 1*/ 
-//const button1 = document.querySelector("#btn1");
+/*initialisation for input variuables*/ 
 const input1 = document.querySelector("#problem1");// initializing user input 
 const output1 = document.querySelector("#message"); // initializing 
 
 
-
-let sumP1=0;
-
 //fuction to solve the first problem
-
-
 function multiples() { 
-
-   
+    let sumP1=0;
    //output1.innerHTML = "The answer is: ";//displaying output
-     for (let count = 0; count < input1.value; count++) {
-        if (count % 3 === 0|| count % 5 === 0) {
+     for (let count = 0; count < input1.value; count++) { //used the input from user as my countvalue
+        if (count % 3 === 0 || count % 5 === 0) { // 
              sumP1 += count;
          }
         
     }
-     output1.innerHTML = "The sum of " + input1.value + " = " + sumP1;
+     output1.innerHTML = "The sum of " + input1.value + " = " + sumP1; //displaying of my sum and output
 }
+
+
+//initialization
+const input2 = document.querySelector(#problem2);
+const output2 = document.querySelector(#message);
 
 //function to solve the second solution
 function fibonacci() {
@@ -30,6 +28,9 @@ function fibonacci() {
 }
 
 
+//initialization for the third problem
+const input3 = document.querySelector(#problem3);
+const input4 = document.querySelector(#message);
 //function to solve the third solution
 function primeFactor() {
    
